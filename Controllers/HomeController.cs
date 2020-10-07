bc10424ce9328bfcc2aa679fd1cf8f29caa04cef
Kent Pedersen <webzone@kpdesign.dk>
2020-10-07 10:49:32 +0200

    fixed minor bugs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -29,11 +29,11 @@ namespace Hotel_landlyst_v_0_01.Controllers
         {
             this.configuration = config;
         }
-        //
-        //public HomeController(ILogger<HomeController> logger)
-        //{
-        //    _logger = logger;
-        //}
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
         #endregion
 
         public IActionResult Index()
@@ -62,41 +62,9 @@ namespace Hotel_landlyst_v_0_01.Controllers
             SearchListModel returnedList;
             DALReservation dr = new DALReservation(configuration);
             returnedList= dr.SearchRooms(searchInput);
-            //RoomModel searchResult = dr.SearchRooms(searchInput);
-            //return View(RoomModel.searchResult);
             List<RoomModel> test = returnedList.AccessList();
             return View(test);
         }
-
-
-
-
-        //public IActionResult SearchRoomsResults(SearchRoomsModel searchInput)
-        //{
-
-        //DALReservation dr = new DALReservation(configuration);
-        //    //RoomModel roomList = dr.SearchRooms(searchInput);
-        //    dr.SearchRooms(searchInput);
-        //    //RoomModel searchResult = dr.SearchRooms(searchInput);
-        //    //return View(RoomModel.searchResult);
-
-        //    return View(roomList);
-        //}
-
-
-
-
-        //  public IActionResult SearchRoomsResults(SearchRoomsModel searchInput)
-        //{
-        //      List<RoomModel> returnroomList = new List<RoomModel>();
-        //DALReservation dr = new DALReservation(configuration);
-        //    //RoomModel roomList = dr.SearchRooms(searchInput);
-        //    returnroomList=dr.SearchRooms(searchInput);
-        //    //RoomModel searchResult = dr.SearchRooms(searchInput);
-        //    //return View(RoomModel.searchResult);
-
-        //    return View(List<>);
-        //}
 
         public IActionResult Booking()
         {
