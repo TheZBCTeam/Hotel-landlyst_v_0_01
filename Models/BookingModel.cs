9467cd6f1f0b16b69fa8c6db9e2c3d9d2955d153
Kent Pedersen <webzone@kpdesign.dk>
2020-10-09 08:39:41 +0200

    Revert "The roomSearch now returns the roomId to the controller to be used in the booking"
    This reverts commit 2d52d01690dc8a914083827cd6b448c51c6389dc.

--- Models/BookingModel.cs
+++ Models/BookingModel.cs
@@ -9,160 +9,54 @@ namespace Hotel_landlyst_v_0_01.Models
     //Logic to add Labels to the formfields, and to apply fieldcontrol to the fields
     public class BookingModel
     {
-        #region Fields
-
-
-        private int customerId;
-        private int roomId;
-        private string firstName = null;
-        private string lastName = null;
-        private string streetName = null;
-        private int streetNumber;
-        private int zipPostal;
-        private string city = null;
-        private string country = null;
-        private string phoneNumber = null;
-        private string email = null;
-        private string confirmEmail = null;
-
-        #endregion
-
-
 
         #region Properties
 
-        [Display(Name = "Customer ID: ")]
-        public int CustomerID
-        {
-            get { return customerId; }
-            set { customerId = value; }
-        }
-
-        [Display(Name = "Room ID: ")]
-        public int RoomId
-        {
-            get { return roomId; }
-            set { roomId = value; }
-        }
+        [Display(Name = "Reservation ID: ")]
+        public int ReservationID { get; set; }
 
         [Display(Name = "Fornavn: ")]
         [Required(ErrorMessage = "Udfyld venligst Dit fornavn.")]
-        public string FirstName
-        {
-            get { return firstName; }
-            set { firstName = value; }
-        }
+        public string FirstName { get; set; }
 
         [Display(Name = "Efternavn: ")]
         [Required(ErrorMessage = "Udfyld venligst Dit efternavn.")]
-        public string LastName
-        {
-            get { return lastName; }
-            set { lastName = value; }
-        }
+        public string LastName { get; set; }
 
         [Display(Name = "Vejnavn: ")]
         [Required(ErrorMessage = "Udfyld venligst vejnavn.")]
-        public string StreetName
-        {
-            get { return streetName; }
-            set { streetName = value; }
-        }
+        public string StreetName { get; set; }
 
         [Display(Name = "Husnummer: ")]
         [Required(ErrorMessage = "Udfyld venligst Husnummer.")]
-        public int StreetNumber
-        {
-            get { return streetNumber; }
-            set { streetNumber = value; }
-        }
+        public int StreetNumber { get; set; }
 
         [Display(Name = "Postnummer: ")]
         [Range(1000, 9999, ErrorMessage = "Ugyldigt postnummer.")]
-        public int ZipPostal
-        {
-            get { return zipPostal; }
-            set { zipPostal = value; }
-        }
+        public int ZipPostal { get; set; }
 
         [Display(Name = "By: ")]
         [Required(ErrorMessage = "Udfyld venligst bynavn.")]
-        public string City
-        {
-            get { return city; }
-            set { city = value; }
-        }
+        public string City { get; set; }
 
         [Display(Name = "Land: ")]
         [Required(ErrorMessage = "Udfyld venligst Land.")]
-        public string Country
-        {
-            get { return country; }
-            set { country = value; }
-        }
+        public string Country { get; set; }
 
         [Display(Name = "Telefon: ")]
         [Required(ErrorMessage = "Udfyld venligst telefonnummer.")]
-        public string PhoneNumber
-        {
-            get { return phoneNumber; }
-            set { phoneNumber = value; }
-        }
+        public string PhoneNumber { get; set; }
 
         [DataType(DataType.EmailAddress)]
         [Display(Name = "Email: ")]
         [Required(ErrorMessage = "Udfyld venligst din email addresse.")]
-        public string Email
-        {
-            get { return email; }
-            set { email = value; }
-        }
+        public string Email { get; set; }
 
         [Display(Name = "Bekræft Email: ")]
         [Compare("Email", ErrorMessage = "Email addressen stemmer ikke overens med den først indtastede.")]
-        public string ConfirmEmail
-        {
-            get { return confirmEmail; }
-            set { confirmEmail = value; }
-        }
+        public string ConfirmEmail { get; set; }
 
 
-
-        #region Constructors
-        public BookingModel()
-        {
-
-        }
-
-        public BookingModel(
-            int customerId,
-            int roomId,
-            string firstName,
-            string lastName,
-            string streetName,
-            int streetNumber,
-            int zipPostal,
-            string city,
-            string country,
-            string phoneNumber,
-            string email,
-            string confirmEmail
-            )
-        {
-            this.customerId = customerId;
-            this.roomId = roomId;
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.streetName = streetName;
-            this.streetNumber = streetNumber;
-            this.zipPostal = zipPostal;
-            this.city = city;
-            this.country = country;
-            this.phoneNumber = phoneNumber;
-            this.email = email;
-            this.confirmEmail = confirmEmail;
-        }
-        #endregion
 
 
 
